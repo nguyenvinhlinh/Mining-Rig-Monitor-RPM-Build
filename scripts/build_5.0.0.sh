de122c33a327f50740a8d91b63a3174381b8969f
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-06-02 11:17:35 +0700

    add rpm build for version 5.0.0

---
+++ scripts/build_5.0.0.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+export RPM_BUILD_DIR=/home/nguyenvinhlinh/Projects/Mining-Rig-Monitor/mining_rig_monitor_rpmbuild
+
+cd /tmp;
+rm -rf /tmp/mining_rig_monitor-5.0.0;
+rm -rf /tmp/mining_rig_monitor-5.0.0.tar.xz;
+rm -rf $RPM_BUILD_DIR/SOURCES/mining_rig_monitor-5.0.0.tar.xz;
+
+git clone --depth 1 --branch release/v5.0.0 git@github.com:nguyenvinhlinh/Mining-Rig-Monitor.git mining_rig_monitor-5.0.0;
+tar -cJf mining_rig_monitor-5.0.0.tar.xz mining_rig_monitor-5.0.0;
+cp /tmp/mining_rig_monitor-5.0.0.tar.xz $RPM_BUILD_DIR/SOURCES/;
+
+cd $RPM_BUILD_DIR/SPECS;
+export QA_RPATHS=$(( 0x0001|0x0002 ))
+rpmbuild --define "_topdir $RPM_BUILD_DIR"  --bb mining_rig_monitor-5.0.0.spec;
